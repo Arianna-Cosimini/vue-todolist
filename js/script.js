0d0c03899dace2ac30bb44aaedd7a38ed9f5c0b0
arianna-cosimini <arianna.cosimini@gmail.com>
2024-02-20 14:04:38 +0100

    Creato array di oggetti

--- js/script.js
+++ js/script.js
@@ -5,7 +5,35 @@ const { createApp } = Vue
 createApp({
     data() {
         return {
+            toDoList: [
+                {
+                    text: "Fare la spesa",
+                    done: false,
+                },
+                {
+                    text: "Comprare mangiare cani",
+                    done: true,
+                },
+                {
+                    text: "Ripassare concetti vue",
+                    done: false,
+                },
+                {
+                    text: "Comprare biscotti per asilo",
+                    done: true,
+                },
+                {
+                    text: "Fare lavatrice",
+                    done: false,
+                },
+                {
+                    text: "Svuotare lavastoviglie",
+                    done: true,
+                },
+            ]
 
+            
+            
         }
 
 
